86022e1b6b575a6c12add5cb5273737bd3e0ab52
Stefany Sato <stefanysato@gmail.com>
2026-06-08 15:02:43 -0300

    disable pwa

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { FavoritesProvider } from './context/FavoritesContext.jsx'
-import { registerSW } from 'virtual:pwa-register'
+// import { registerSW } from 'virtual:pwa-register'
 
-registerSW({
-  immediate: true
-})
+// registerSW({
+//   immediate: true
+// })
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
